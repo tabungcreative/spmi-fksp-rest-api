204df5fa5038b88cf92c3ad1ca750c88c9551bf9
ahmadrifai <rifai0850@gmail.com>
2022-03-05 11:36:38 +0700

    first commit

---
+++ src/test/java/id/ac/fksp/spmi/spmirestfullapi/SpmiRestfullApiApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.fksp.spmi.spmirestfullapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpmiRestfullApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
